ef1eee10f29400d08f896eda8b4dcaca4af21ca9
dariosotovs <dario.soto@vascarsolutions.com>
2019-06-01 12:24:15 -0400

    set heroku vars

--- config.js
+++ config.js
@@ -5,8 +5,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'local') {
   urlDB = 'mongodb://localhost:27017/coffee';
 } else {
-  urlDB =
-    'mongodb+srv://dariosoto:13121987xd@daario-vfcs8.mongodb.net/coffee?retryWrites=true&w=majority';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
