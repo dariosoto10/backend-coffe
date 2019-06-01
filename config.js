d45247c86fd2d722e4dbfdb984c3c0f439b5fbae
dariosotovs <dario.soto@vascarsolutions.com>
2019-06-01 12:38:56 -0400

    fix issue

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'local') {
   urlDB = 'mongodb://localhost:27017/coffee';
 } else {
   urlDB = process.env.MONGO_URI;
